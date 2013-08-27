9f6b43f0c94377db8859e8910ed271ed1ffdff6b
Piotr Dobrowolski <admin@tastycode.pl>
2013-08-27 13:33:28 +0200

    Initial commit

---
+++ nextbike2json.py
@@ -0,0 +1,20 @@
+from xml.dom import minidom
+import sys
+import json
+
+if len(sys.argv) < 2:
+    print >>sys.stderr, 'usage: python %s data.xml' % sys.argv[0]
+    print >>sys.stderr, '\nWarsaw Veturilo: https://www.nextbike.pl/maps/nextbike-official.xml?city=210'
+    exit(1)
+
+tree = minidom.parse(sys.argv[1])
+places_list = {}
+for place_elm in tree.childNodes[0].getElementsByTagName("place"):
+    place = {"lat": place_elm.getAttribute("lat"),
+             "lng": place_elm.getAttribute("lng"),
+             "name": place_elm.getAttribute("name").strip(),
+             "uid": int(place_elm.getAttribute("uid"))}
+
+    places_list[place['uid']] = place
+
+print json.dumps(places_list)
